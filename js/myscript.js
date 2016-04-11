98c52b9736099fc7c8b1c031c17ed4cdef0c9272
Uku <Ukusaarm@gmail.com>
2016-04-11 04:05:47 +0300

    js animations and content hiding

--- js/myscript.js
+++ js/myscript.js
@@ -5,10 +5,45 @@ $(function() {
   });
 
   $(".nav-handle").click(function() {
-			$(this).toggleClass("open");
-		});
+    $(this).toggleClass("open");
+  });
 
-    $(".nav-handle").click(function() {
-        $(".site-mask").toggleClass("active");
-      });
+  $(".nav-handle").click(function() {
+    $(".site-mask").toggleClass("active");
+  });
+
+  $('#sth-div').show(1000);
+  $('#sth1-div').hide();
+  $('#sth2-div').hide();
+  $('#sth3-div').hide();
+
+  $('#sth').click(function() {
+    $('#sth-div').show(1000);
+    $('#sth1-div').hide();
+    $('#sth2-div').hide();
+    $('#sth3-div').hide();
+  });
+
+  $('#sth1').click(function() {
+    $('#sth1-div').show();
+    $('#sth-div').hide();
+    $('#sth2-div').hide();
+    $('#sth3-div').hide();
+  });
+
+  $('#sth2').click(function() {
+    $('#sth2-div').show();
+    $('#sth-div').hide();
+    $('#sth1-div').hide();
+    $('#sth3-div').hide();
+  });
+
+  $('#sth3').click(function() {
+    $('#sth3-div').show();
+    $('#sth-div').hide();
+    $('#sth1-div').hide();
+    $('#sth2-div').hide();
+  });
+
+
 });
